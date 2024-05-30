f114ec98684e8b78c8f0ec55edab120c68e9ad4d
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-05-30 16:18:01 -0400

    import icons for PasswordInput

--- frontend/src/components/Input/PasswordInput.jsx
+++ frontend/src/components/Input/PasswordInput.jsx
@@ -1,4 +1,5 @@
 import React, { useState} from 'react';
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa6';
 
 const PasswordInput = ({ value, onChange, placeholder }) => {
   const [isShowPassword, setIsShowPassword] = useState(false);
@@ -16,6 +17,22 @@ const PasswordInput = ({ value, onChange, placeholder }) => {
         placeholder={placeholder || "Password"}
         className="w-full text-sm bg-transparent py-3 mr-3 rounded outline-none"
       />
+      {
+        isShowPassword ? (
+          <FaRegEye 
+            size={22} 
+            className="text-primary cursor-pointer" 
+            onClick={() => toggleShowPassword()}
+          />
+        ) : (
+          <FaRegEyeSlash
+            size={22} 
+            className="text-slate-400 cursor-pointer" 
+            onClick={() => toggleShowPassword()}
+          />
+        )
+      }
+
     </div>
   )
 }
