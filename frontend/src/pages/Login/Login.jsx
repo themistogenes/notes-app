abedaa13e89faca0ec3f5f9ec330022d8461eacf
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-05-30 16:01:10 -0400

    create PasswordInput component

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import PasswordInput from '../../components/Input/PasswordInput';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
@@ -12,11 +13,8 @@ const Login = () => {
             onSubmit={() => {}}  
           >
             <h4 className="text-2xl mb-7">Login</h4>
-            <input 
-              type="text" 
-              placeholder="Email" 
-              className="input-box"
-            />
+            <input type="text" placeholder="Email" className="input-box"/>
+            <PasswordInput />
             <button type="submit" className="btn-primary">
               Login
             </button>
