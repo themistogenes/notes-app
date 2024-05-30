4aa70ad0dcdd67710fe9cdcf4a13c31ca8a4911f
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-05-30 16:33:10 -0400

    Login state and validations

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import PasswordInput from '../../components/Input/PasswordInput';
 import { Link } from 'react-router-dom';
+import { validateEmail } from '../../utils/helper';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError('Please enter a valid email address.');
+      return;
+    }
+
+    if (!password) {
+      setError('Please enter the password.');
+      return;
+    }
+
+    setError('');
+
+    // Login API call
+  }
 
   return (
     <>
@@ -11,14 +33,31 @@ const Login = () => {
       <div className="flex items-center justify-center mt-28">
         <div className="w-96 border rounded bg-white px-7 py-10">
           <form
-            onSubmit={() => {}}  
+            onSubmit={handleLogin}  
           >
             <h4 className="text-2xl mb-7">Login</h4>
-            <input type="text" placeholder="Email" className="input-box"/>
-            <PasswordInput />
+
+            <input 
+              type="text" 
+              placeholder="Email" 
+              className="input-box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            {
+              error && <p className="text-red-500 text-xs pb-1">{error}</p>
+            }
+
             <button type="submit" className="btn-primary">
               Login
             </button>
+
             <p className="text-sm text-center mt-4">
               Not registered yet? 
               {' '}
@@ -26,6 +65,7 @@ const Login = () => {
                 Create an Account
               </Link>
             </p>
+
           </form>
         </div>
       </div>
