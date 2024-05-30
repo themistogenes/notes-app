e888a301b3c17eb39b5a27ee75d7bef213326f84
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-05-30 16:09:02 -0400

    PasswordInput component

--- frontend/src/components/Input/PasswordInput.jsx
+++ frontend/src/components/Input/PasswordInput.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState} from 'react';
 
-const PasswordInput = () => {
+const PasswordInput = ({ value, onChange, placeholder }) => {
+  const [isShowPassword, setIsShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setIsShowPassword(!isShowPassword);
+  }
+
   return (
-    <div>PasswordInput</div>
+    <div className="flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3">
+      <input
+        type={isShowPassword ? "text" : "password"}
+        value={value} 
+        onChange={onChange}
+        placeholder={placeholder || "Password"}
+        className="w-full text-sm bg-transparent py-3 mr-3 rounded outline-none"
+      />
+    </div>
   )
 }
 
