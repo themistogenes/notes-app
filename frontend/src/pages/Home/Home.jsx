303bbb18eb92ed1500dd07ab3de9470a17688075
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-05-30 21:55:38 -0400

    update Modal, AddEditNotes, TagInput

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MdAdd } from 'react-icons/md';
 import Navbar from '../../components/Navbar/Navbar';
 import NoteCard from '../../components/Cards/NoteCard';
 import AddEditNotes from './AddEditNotes';
-import { MdAdd } from 'react-icons/md';
+import Modal from 'react-modal';
 
 const Home = () => {
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShown: false,
+    type: 'add',
+    data: null
+  });
+
   return (
     <>
     <Navbar />
@@ -25,11 +32,35 @@ const Home = () => {
     
     <button 
       className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10" 
-      onClick={() => {}}>
+      onClick={() => {
+        setOpenAddEditModal({ isShown: true, type: 'add', data: null })
+      }}>
       <MdAdd className="text-[32px] text-white" />
     </button>
 
-    <AddEditNotes />
+    <Modal
+      isOpen={openAddEditModal.isShown}
+      onRequestClose={() => {}}
+      style={{
+        overlay: {
+          backgroundColor: "rgba(0,0,0,0.2)"
+        }
+      }}
+      contentLabel=""
+      className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+    >
+      <AddEditNotes 
+        type={openAddEditModal.type}
+        noteData={openAddEditModal.data}
+        onClose={() => {
+          setOpenAddEditModal({
+            isShown: false,
+            type: 'add',
+            data: null
+          })
+        }}
+      />
+    </Modal>
     </>
   )
 }
