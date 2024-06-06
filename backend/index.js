418b0c1f9ca9bb35c7f3bbc24513c453ca232c86
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-06-05 22:12:53 -0400

    update create account API

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 
 mongoose.connect(config.connectionString);
 
+const User = require('./models/user.model.js');
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -45,6 +47,29 @@ app.post('/create-account', async (req, res) => {
       .status(400)
       .json({ error: true, message: 'Password is required'});
   }
+
+  const isUser = await User.findOne({email: email});
+
+  if (isUser) {
+    return res.json({ error: true, message: 'User already exists'});
+  }
+
+  const user = new User({
+    fullName,
+    email,
+    password
+  })
+
+  await user.save();
+
+  const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '36000m'});
+
+  return res.json({
+    error: false,
+    user,
+    accessToken,
+    message: 'Registration successful'
+  })
 })
 
 app.listen(8000);
