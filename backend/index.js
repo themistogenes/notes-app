94c665d9a4360f074dc051ca464e53e85fb5055b
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-06-05 23:05:36 -0400

    add note API

--- backend/index.js
+++ backend/index.js
@@ -110,8 +110,36 @@ app.post('/login', async (req, res) => {
 
 // Add Note
 app.post('/add-note', authenticateToken, async (req, res) => {
+  const { title, content, tags } = req.body;
+  const { user } = req.user;
 
-} )
+  if (!title) {
+    return res.status(400).json({ error: true, message: 'Title is required'});
+  }
+
+  if (!content) {
+    return res.status(400).json({ error: true, message: 'Content is required'});
+  }
+
+  try {
+    const note = new Note({
+      title,
+      content,
+      tags: tags || [],
+      userId: user._id
+    })
+
+    await note.save();
+
+    return res.json({
+      error: false,
+      note,
+      message: 'Note added successfully'
+    })
+  } catch (error) {
+    return res.status(500).json({ error: true, message: 'Internal server error'});
+  }
+})
 
 app.listen(8000, () => {
   console.log('Server is running on port 8000');
