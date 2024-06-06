fb5dfdd94181514d0d16c8cf89b0d766b6993c8a
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-06-05 21:43:47 -0400

    require mongoose and jwt

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
+require('dotenv').config();
+
+const config = require('./config.json');
+const mongoose = require('mongoose');
+
+mongoose.connect(config.connectionString);
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
+const jwt = require('jsonwebtoken');
+const { authenticateToken } = require('./utilities.js');
 
 app.use(express.json());
 
